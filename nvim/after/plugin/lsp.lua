ce95513027a4f12c8cd60077c963b29f49870a03
William Banfield <wbanfield@gmail.com>
2022-12-18 15:16:01 -0500

    add lua config for lsp

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,15 @@
+local on_attach = function(client, bufnr) 
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, nil)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, nil)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, nil)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, nil)
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig')['gopls'].setup{
+	on_attach = on_attach,
+	flags = lsp_flags,
+	capabilities = capabilities
+}
+
