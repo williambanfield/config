4b91919374c9a69ad920542d0dab82965b2e9cae
William Banfield <william.banfield@chronosphere.io>
2023-10-15 11:39:32 -0400

    fix diag from being jumped to

--- nvim/after/plugin/diag.lua
+++ nvim/after/plugin/diag.lua
@@ -3,7 +3,7 @@ vim.diagnostic.config({
 	virtual_text = false,
 })
 
-vim.cmd('autocmd CursorHold * lua vim.diagnostic.open_float()') 
+vim.cmd('autocmd CursorHold * lua vim.diagnostic.open_float({ focusable = false })')
 vim.o.updatetime = 300
 
 
