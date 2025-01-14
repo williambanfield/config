f299229aa1f4bf18f05a7c4ef32d15fc72f08759
William Banfield <william.banfield@chronosphere.io>
2025-01-14 00:38:37 +0000

    lualine shows relative path

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,47 +1,8 @@
 require('lualine').setup({
+	sections = {
+		lualine_c = { { 'filename', path = 1 } }
+	},
 	options = {
 		theme = 'gruvbox',
 	}
 })
-
-require('symbols-outline').setup({
-	highlight_hovered_item = true,
-	show_guides = true,
-	auto_preview = false,
-	position = 'right',
-	symbols = {
-		File = {icon = "", hl = "@text.uri"},
-		Module = {icon = "󰆧", hl = "@namespace"},
-		Namespace = {icon = "󰅪", hl = "@namespace"},
-		Package = {icon = "󰏗", hl = "@namespace"},
-		Class = {icon = "󰬊", hl = "@type"},
-		Method = {icon = "󰰐", hl = "@method"},
-		Property = {icon = "", hl = "@method"},
-		Field = {icon = "󰆨", hl = "@field"},
-		Constructor = {icon = "", hl = "@constructor"},
-		Enum = {icon = "󰷐", hl = "@type"},
-		Interface = {icon = "󰠲", hl = "@type"},
-		Function = {icon = "", hl = "@function"},
-		Variable = {icon = "", hl = "@constant"},
-		Constant = {icon = "", hl = "@constant"},
-		String = {icon = "󰅳", hl = "@string"},
-		Number = {icon = "", hl = "@number"},
-		Boolean = {icon = "󰬉", hl = "@boolean"},
-		Array = {icon = "󰅪", hl = "@constant"},
-		Object = {icon = "⦿", hl = "@type"},
-		Key = {icon = "", hl = "@type"},
-		Null = {icon = "󰟢", hl = "@type"},
-		Operator = { icon = "+", hl = "@operator" },
-		EnumMember = {icon = "", hl = "@field"},
-		Struct = {icon = "󰅩", hl = "@type"},
-	},
-	autofold_depth = 1,
-})
-
-vim.cmd [[
-  augroup OpenSymbolsOutline
-    autocmd!
-	autocmd BufEnter *.go lua require('symbols-outline').open_outline()
-
-  augroup END
-]]
