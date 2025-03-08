9536f8a4d0e5968d25f05952c7648d913668658d
William Banfield <william.banfield@chronosphere.io>
2025-03-08 19:53:36 +0000

    add commands to search and change to current dir

--- nvim/lua/environment/editor.lua
+++ nvim/lua/environment/editor.lua
@@ -30,4 +30,31 @@ vim.opt.shiftwidth=4
 vim.opt.listchars={eol='¬',tab='>·',trail='~',extends='>',precedes='<',space='_'}
 vim.opt.list=true
 vim.opt.swapfile=false
+vim.opt.shortmess:remove("F")
+vim.opt.statusline = "%F"
 vim.g.vim_markdown_folding_disabled=1
+
+
+-- Open the directory of the current file
+vim.api.nvim_create_user_command("Dir", function(opts)
+    vim.cmd('edit ' .. vim.fn.expand('%:p:h'))
+end, {})
+
+vim.api.nvim_create_user_command("RRoot", function(opts)
+    local output = vim.fn.systemlist("git rev-parse --show-toplevel")
+    vim.cmd('edit ' .. output[1])
+end, {})
+
+vim.api.nvim_create_user_command("RgHere", function(opts)
+    local local_dir = vim.fn.expand('%:h')
+    if local_dir == "." then
+        local_dir = ""
+    end
+    local cmd = string.format("rg --column --line-number --no-heading --color=always -g'%s/**' --smart-case -- %s", local_dir, vim.fn.shellescape(opts.args))
+    local is_bang = 0
+  
+    vim.fn["fzf#vim#grep"](cmd, vim.fn["fzf#vim#with_preview"](), is_bang)
+  end, {
+    nargs = "*",
+    desc = "Search the directory of the current file with ripgrep + fzf"
+  })
